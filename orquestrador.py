c5b32ab958b8150a087e8781eeca1602451f9240
adriannylelis <adrianny.lelis@gmail.com>
2025-10-30 18:16:43 -0400

    feat: Add FerramentaExplicadora to AgenteOrquestrador for enhanced content explanation capabilities

--- orquestrador.py
+++ orquestrador.py
@@ -8,6 +8,7 @@ from langchain import hub
 from langchain.agents import create_react_agent
 from langchain.agents import Tool
 from ferramenta_analisadora_imagem import FerramentaAnalisadoraImagem
+from ferramenta_explicadora import FerramentaExplicadora
 
 class AgenteOrquestrador:
     def __init__(self):
@@ -17,7 +18,7 @@ class AgenteOrquestrador:
         )
 
         ferramenta_analisadora_imagem = FerramentaAnalisadoraImagem()
-
+        ferramenta_explicadora = FerramentaExplicadora()
 
         self.tools = [
             Tool(
@@ -25,6 +26,12 @@ class AgenteOrquestrador:
                 func= ferramenta_analisadora_imagem.run,
                 description= ferramenta_analisadora_imagem.description,
                 return_direct= ferramenta_analisadora_imagem.return_direct
+            ),
+            Tool(
+                name= ferramenta_explicadora.name,
+                func= ferramenta_explicadora.run,
+                description= ferramenta_explicadora.description,
+                return_direct= ferramenta_explicadora.return_direct
             )
         ]
 
